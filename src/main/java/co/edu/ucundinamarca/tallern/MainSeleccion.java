66571706495359da315f272987a581f4de7a57ea
ximen <ximen@DESKTOP-ST73CCB>
2021-09-12 12:11:57 -0500

    Taller2

--- src/main/java/co/edu/ucundinamarca/tallern/MainSeleccion.java
+++ src/main/java/co/edu/ucundinamarca/tallern/MainSeleccion.java
@@ -12,25 +12,23 @@ import Clases.SeleccionColombia;
  * @author A_Ximena_Vanegas_M
  */
 public class MainSeleccion {
-    
+
     public static void main(String[] args) {
-        
-        SeleccionColombia jugadores =new SeleccionColombia();
-        
+
+        SeleccionColombia jugadores = new SeleccionColombia();
+
         //jugadores agregados para probar el programa
         jugadores.agregarJugadores("hugo");
         jugadores.agregarJugadores("paco");
         jugadores.agregarJugadores("luis");
-        
+
         //mostrar todos los jugadores
         jugadores.mostrarJugadores();
-        
+
         //buscar y verificar si exixte un jugador
         jugadores.contieneJugador("lucho");//en este caso no existe
         jugadores.contieneJugador("paco");//en este caso si existe
-        
-        
-        
+
     }
-    
+
 }

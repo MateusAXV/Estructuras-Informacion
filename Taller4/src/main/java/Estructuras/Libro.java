f9552d51f22ff3268eec587b7475acf405b859e3
ximen <ximen@DESKTOP-ST73CCB>
2021-09-29 14:14:05 -0500

    Taller 4-Listas

--- Taller4/src/main/java/Estructuras/Libro.java
+++ Taller4/src/main/java/Estructuras/Libro.java
@@ -13,4 +13,25 @@ public class Libro {
 
     private String titulo, autor;
 
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
 }

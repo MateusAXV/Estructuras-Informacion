594d205eeb101c785bfae61895ecb9bb9fafd72a
ximen <ximen@DESKTOP-ST73CCB>
2021-10-15 15:14:50 -0500

    Taller 5- lista circular simple

--- Taller5/src/main/java/lista/listaCircular.java
+++ Taller5/src/main/java/lista/listaCircular.java
@@ -5,13 +5,47 @@
  */
 package lista;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author A_Ximena_Vanegas_M
  */
 public class listaCircular {
     
+    Nodo ultimo;
+    static Logger log = Logger.getLogger(listaCircular.class.getName());
     
+    public listaCircular() {
+        ultimo = null;
+    }
+
+    //metodo para saber si la lista esta vacia
+    public boolean estaVacia() {
+        return ultimo == null;
+    }
+
+    //Metodo para insertar Nodos
+    public listaCircular insertar(int elemento) {
+        Nodo nuevo = new Nodo(elemento);
+        if (ultimo != null) {
+            nuevo.siguiente = ultimo.siguiente;
+            ultimo.siguiente = nuevo;
+        }
+        ultimo = nuevo;
+        return this;
+    }
+
+    //metodo para mostrar la lista
+    public void mostrarLista() {
+        Nodo aux = ultimo.siguiente;
+        String cadena = "";
+        do {
+            cadena = cadena + "[" + aux.dato + "]->";
+        } while (aux != ultimo.siguiente);
+        log.info(cadena);        
+    }
     
+    //19.54
     
 }

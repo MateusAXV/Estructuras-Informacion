8edc572cb65226a6e2af20c026a6d07a4630b782
ximen <ximen@DESKTOP-ST73CCB>
2021-10-22 16:24:32 -0500

    taller 6

---
+++ Taller6/src/main/java/Lista/nodo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista;
+
+/**
+ *
+ * @author A_Ximena_Vanegas_M
+ */
+public class nodo {//nodo para la lista circular
+    int datos;
+    nodo siguiente;
+    nodo anterior;
+    
+}

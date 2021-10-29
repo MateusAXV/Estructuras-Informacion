e7168148e19cd5111ab486e3a46eee807a735476
ximen <ximen@DESKTOP-ST73CCB>
2021-10-29 17:51:57 -0500

    parcial punto 3

--- Parcial/src/main/java/punto3/Principal.java
+++ Parcial/src/main/java/punto3/Principal.java
@@ -7,14 +7,48 @@ package punto3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author A_Ximena_Vanegas_M
  */
 public class Principal {
-   List<String> soldados = new ArrayList<>();
-   
-   
-  
+
+    public static List<String> soldados = new ArrayList<>();
+    public static int numero = 4;
+    static Logger log = Logger.getLogger(Principal.class.getName());
+
+    public static void main(String[] args) {
+
+        //nombre soldaddos (8 en total)
+        soldados.add("Francisco");
+        soldados.add("Jorge");
+        soldados.add("Luis");
+        soldados.add("Sergio");
+        soldados.add("Ricardo");
+        soldados.add("Salvador");
+        soldados.add("Diego");
+        soldados.add("Mario");
+
+        log.info("---NOMBRE DE LOS SOLDADOS---");
+        for (int i = 0; i < soldados.size(); i++) {
+            log.info(soldados.get(i));
+        }
+
+        log.info("El soldado que va a pedir ayuda es: "
+                + soldados.get(rta(numero, soldados.size())));
+
+    }
+
+    public static int rta(int num, int size) {
+
+        if (size == 1) {
+            return 1;
+        } else {
+            return (rta(size - 1, num) + num - 1) % size + 1;
+        }
+    }
+
 }

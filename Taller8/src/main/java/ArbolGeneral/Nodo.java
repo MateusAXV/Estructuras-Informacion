b0ae474a15b28e4bb32264f1865bcef487050d6e
ximen <ximen@DESKTOP-ST73CCB>
2021-11-12 14:28:33 -0500

    Taller 8 - Arbol n-Ario (general)

---
+++ Taller8/src/main/java/ArbolGeneral/Nodo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArbolGeneral;
+
+/**
+ *
+ * @author A_Ximena_Vanegas_M
+ */
+public class Nodo {
+
+    String info;
+    int nHijos;
+    Nodo hijos[];
+    Nodo hijosT[];
+
+    public Nodo(String dato) {
+        info = dato;
+        this.nHijos = 0;
+    }
+    
+    
+
+}
